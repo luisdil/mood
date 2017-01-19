2a5e83de6816b9141cacf00dffc5d08b208f0d35
Luis Dille <luis-dille@live.de>
2017-01-19 20:39:08 +0100

    first commit

---
+++ Mood/Brain.swift
@@ -0,0 +1,9 @@
+//
+//  Brain.swift
+//  Mood
+//
+//  Created by Luis Dille on 15.01.17.
+//  Copyright © 2017 Luis Dille. All rights reserved.
+//
+
+import Foundation
---
+++ Mood/moodandtime.swift
@@ -0,0 +1,13 @@
+//
+//  moodandtime.swift
+//  Mood
+//
+//  Created by Luis Dille on 19.01.17.
+//  Copyright © 2017 Luis Dille. All rights reserved.
+//
+
+import UIKit
+
+class moodandtime: NSObject {
+
+}
